b9f4245eb9dc0e6e59f5370ba8dc1d894b5f018d
Kevin <chy.17@outlook.com>
2018-06-06 18:57:41 +0800

    20180606

---
+++ SearchTest.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumPrepare4Crawler
+{
+    [TestClass]
+    public class SearchTest
+    {
+        //[TestMethod]
+        //public void TestMethod1()
+        //{
+        //    IWebDriver webDriver=new FirefoxDriver();
+        //    webDriver.Navigate().GoToUrl("https://www.google.com");
+        //    IWebElement webElement = webDriver.FindElement(By.Name("q"));
+        //    webElement.SendKeys("facebook");
+        //    webElement.Submit();
+        //    System.Threading.Thread.Sleep(1000);
+        //    webDriver.Quit();
+
+        //}
+
+        //[TestMethod]
+        //public void TestEdge()
+        //{
+
+        //    IWebDriver driver = new EdgeDriver();
+        //    driver.Navigate().GoToUrl("https://www.bing.com");
+        //    var src = driver.PageSource;
+        //    IWebElement webElement = driver.FindElement(By.Name("q"));
+        //    webElement.SendKeys("facebook");
+        //    webElement.Submit();
+        //    System.Threading.Thread.Sleep(1000);
+        //   // driver.Quit();
+        //}
+
+        //[TestMethod]
+        //public void TestYahooAutoLogin()
+        //{
+        //    IWebDriver driver = new EdgeDriver();
+        //    driver.Navigate().GoToUrl("https://login.yahoo.com/");
+        //    IWebElement webElement = driver.FindElement(By.Id("login-username"));
+        //    webElement.SendKeys("account");//account field
+        //    System.Threading.Thread.Sleep(1000);
+        //    driver.FindElement(By.Id("login-signin")).Click();
+
+        //    driver.FindElement(By.Id("login-passwd")).SendKeys("password123");//password field
+        //    driver.FindElement(By.Id("login-signin")).Click();
+        //    webElement.Submit();
+        //    driver.Close();
+
+        //}
+
+        /// <summary>
+        /// Tests the flip automatic chat.
+        /// </summary>
+        [TestMethod]
+        public void TestFlipAutoChat()
+        {
+            IWebDriver webDriver = new FirefoxDriver();
+            webDriver.Navigate().GoToUrl("https://fleep.io/chat");
+            webDriver.FindElement(By.XPath("//input[@placeholder='Username or email address']")).SendKeys("gmail.com");
+            webDriver.FindElement(By.XPath("//input[@placeholder='Password']")).SendKeys("password");
+
+            webDriver.FindElement(By.XPath("//button[contains(text(), 'SIGN IN')]")).Click();
+
+            System.Threading.Thread.Sleep(9000);
+            //hard code
+            webDriver.FindElement(By.XPath("//div[contains(text(),'讀書會')]")).Click();
+            var elem = webDriver.FindElement(By.XPath("//textarea[@placeholder='Type your message']"));
+            elem.SendKeys(DateTime.Now.ToLongDateString());
+            elem.FindElement(By.XPath("//div[contains(text(), 'Send')]")).Click();
+            webDriver.Quit();
+            //var elems = webDriver.FindElements(By.ClassName("conversation"));
+            //foreach (var elem in elems)
+            //{
+            //    if (elem.GetAttribute("data-id") == "8ac9096d-41cf-476f-a666-15ef3a469a84")
+            //    {
+            //        elem.Click();
+            //        // ((IJavaScriptExecutor)webDriver).ExecuteScript("arguments[0].click();", elem);
+            //    }
+            //}
+
+        }
+    }
+}
